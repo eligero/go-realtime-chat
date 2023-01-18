f341fb4815e106a488610df5cf3634858c98e7fe
Esteban L. Gómez <esteban.elig@gmail.com>
2023-01-18 22:42:37 +0100

    HTML with templates

--- main.go
+++ main.go
@@ -3,21 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Chat body
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	once       sync.Once
+	filename   string
+	thTemplate *template.Template
+}
+
+// templateHandler method ServeHTTP handles the HTTP request. It satisfies the http.Handler interface
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// sync.Once type guarantees that the function we pass as an argument will only be executed once, regardless of how many goroutines are calling ServeHTTP
+	t.once.Do(func() { // load, compile and execute the template
+		t.thTemplate = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.thTemplate.Execute(w, nil) // Write the output to the ResponseWriter
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// Start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
