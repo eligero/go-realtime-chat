33f71bfd58032278aecf2d0b1be576a01e19bd2e
Esteban L. Gómez <esteban.elig@gmail.com>
2023-01-21 19:49:01 +0100

    Logout

--- chat/main.go
+++ chat/main.go
@@ -57,6 +57,16 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "", // Clear Cookie's value
+			Path:   "/",
+			MaxAge: -1, // Immediately removed by the browser, if supported by browser
+		})
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 
 	// Run the room in a goroutine
 	go r.run()
